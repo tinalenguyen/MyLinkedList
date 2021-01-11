05955ff776f464a78b48ff323d69e9f863a0dbdd
tinalenguyen <>
2021-01-10 23:01:31 -0500

    added remove

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -12,6 +12,17 @@ public class MyLinkedList{
 
  }
 
+ private Node getIndex(int index){
+   if (index == 0) return start;
+   if (index == size-1) return end;
+   int counter = 0 ;
+   Node val = start;
+   while (counter < index){
+     val = val.getNext();
+     counter++;
+   }
+   return val;
+ }
  public boolean add(String value){
   //appends to the end of the linkedlist
    Node input = new Node(value);
@@ -27,58 +38,51 @@ public class MyLinkedList{
       end = input;
 
     }
+    size++;
     return true;
+
  }
 
  public void add(int index, String value){
-   if (index >size || index < 0){
+   if (index > size || index < 0){
      throw new IndexOutOfBoundsException("Index " +index+" is out of bounds");
    }
 
    Node input = new Node(value);
 
-  if (size == 0 || index == size) add(value);
-  if (index == 0){
+  if (size == 0 || index == size){
+    add(value);
+  }else if (index == 0){
     //append to the beginning
     start.setPrev(input);
     input.setNext(start);
     start = input;
   }else{
   Node prevv = new Node(get(index-1) );
-  Node nextt = new Node(get(index+1) );
+  Node nextt = new Node(get(index) );
   prevv.setNext(input);
   nextt.setPrev(input);
   input.setNext( nextt );
   input.setPrev( prevv );
 
   }
-
-
+  size++;
  }
 
  public String get(int index){
     if (index >= size || index < 0){
       throw new IndexOutOfBoundsException("Index " + index + " is out of bounds");
     }
-
-    if (index == 0) return start.getData();
-    if (index == size-1) return end.getData();
-
-    Node val = start;
-    for (int i = 0; i < index; i++){
-      val = val.getNext();
-      i++;
-    }
-    return val.getData();
+    return getIndex(index).getData();
  }
 
  public String set(int index, String value){
    if (index >= size || index < 0){
      throw new IndexOutOfBoundsException("Index " +index+ " is out of bounds");
    }
-   Node prevv = new Node(get(index-2) );
-   Node nextt = new Node(get(index+2) );
-   Node current = new Node(get(index) );
+   Node prevv = getIndex(index-2) ;
+   Node nextt =getIndex(index+2) ;
+   Node current = getIndex(index) ;
    Node val = new Node(value);
 
    if (index == 0) start.setPrev(val);
@@ -98,11 +102,16 @@ public class MyLinkedList{
    String result = "";
 
    Node curr = start;
-   for (int i = 0 ; i < size-1;i++){
-     result += curr.getData() + ", ";
+
+   while (curr != null){
+     if (curr !=start || curr != end){
+       result += curr.getData() + ", ";
+     }
+     else {
+       result += curr.getData();
+     }
      curr = curr.getNext();
    }
-   result += curr.getData();
    return "[" +result+"]";
  }
 
@@ -121,7 +130,38 @@ public class MyLinkedList{
  }
  //Any helper method that returns a Node object MUST BE PRIVATE!
 
+  public String remove(int index){
+    String before = "";
 
+    if (index < 0 || index >= size){
+      throw new IndexOutOfBoundsException("Index " +index+ " is out of bounds");
+    } else if (size == 1){
+      before = start.getData();
+      start = null;
+      end = null;
+    } else if (index == 0){
+      before = start.getData();
+      Node nextt = start.getNext();
+      nextt.setPrev(null);
+      start.setNext(null);
+      start = nextt;
+    } else if (index==size-1){
+      before = end.getData();
+      end = end.getPrev();
+      end.setNext(null);
+    } else {
+      Node oldie = new Node(get(index));
+      before = oldie.getData();
+      oldie.getPrev().setNext(oldie.getNext());
+      oldie.getNext().setPrev(oldie.getPrev());
+      oldie.setNext(null);
+      oldie.setPrev(null);
+
+    }
+
+      size--;
+      return before;
+  }
 
 
 
