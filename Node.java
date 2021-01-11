1924db97ac4ee184a401a5ba8a4ff9c073a3c2fa
tinalenguyen <>
2021-01-10 19:13:43 -0500

    added data methods

--- Node.java
+++ Node.java
@@ -1,7 +1,22 @@
 public class Node {
+  private Node next, prev;
+  private String data;
 
-public Node(String value){/*create a constructor*/}
- private String data;
+//constructor
+ public Node(String value){
+   data = value;
+ }
+
+ //data methods
+ public String getData(){
+   return data;
+ }
+
+ public void setData(String value){
+   data = value;
+ }
+
+
  private Node next,prev;
  //write get/set methods for all three instance variables.
 
